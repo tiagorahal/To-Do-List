858a4cccdb73a9cd3bfb87635236e49f2d4329f0
tiagorahal <rahal.aires@gmail.com>
2021-07-13 22:21:01 -0300

    Fix css structure

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import '@fortawesome/fontawesome-free/js/brands.js';
 
 const list = [
   {
-    description: 'testingOne',
+    description: 'Make eggplant lasagna for lunch',
     completed: true,
     index: 0,
   },
   {
-    description: 'testingTwo',
+    description: 'Finishing the to do list project',
     completed: true,
     index: 1,
   },
   {
-    description: 'testingTres',
+    description: 'Take a shower',
     completed: false,
     index: 2,
   },
@@ -25,7 +25,7 @@ const list = [
 function displayTask() {
   const sortedList = list.sort((a, b) => a.index - b.index);
   for (let i = 0; i <= list.length; i += 1) {
-    document.getElementById('list-items').insertAdjacentHTML('beforeend', `<div class="item-div"><input type="checkbox" id="item-${i}"><p class="task-description">${sortedList[i].description}</p><a href="#"><i class="fas fa-ellipsis-v"></i></a></div>`);
+    document.getElementById('list-items').insertAdjacentHTML('beforeend', `<div class="item-div"><div class="start-task"><input type="checkbox" id="item-${i}"><p class="task-description">${sortedList[i].description}</p></div><a href="#"><i class="fas fa-ellipsis-v"></i></a></div>`);
   }
 }
 
