7d76790b9577353ee4979cccc8e4b889da96bf9a
tiagorahal <rahal.aires@gmail.com>
2021-07-14 22:40:05 -0300

    Add opacity when drag

--- src/drag.js
+++ src/drag.js
@@ -1,5 +1,14 @@
-
-
 export default function callDrag() {
-  console.log('testing 2');
+  const draggables = document.querySelectorAll('.item-div');
+  const container = document.getElementById('list-items');
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging');
+    });
+  });
 }
